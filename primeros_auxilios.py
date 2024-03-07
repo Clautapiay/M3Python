321981281fa14d095280365fd040341e22ebd32c
Claudiatapiay <ctapia.yelpi@gmail.com>
2024-03-07 16:20:09 -0300

    actualización

--- primeros_auxilios.py
+++ primeros_auxilios.py
@@ -3,47 +3,45 @@
 """INICIO"""
 print("Pasos a seguir en primeros auxilios")
 print ("")
-
+print("Debe responder con SI/NO")
 
 respuesta = {"si", "no"}
 
-
-persona = input("¿La persona responde a estimulos? ").lower()
+persona = input("¿La persona responde a estímulos? ").lower()
 print(persona)
 if persona == "si":
-    persona = input("¿Es necesario llevarlo a un hospital? ")
-    print(persona)
-    if persona == "si" or "no":
-        print("Fin ciclo")
+    print("Valorar la necesidad de llevarlo al hospital más cercano")
+    print("FIN CICLO")
 elif persona == "no":
-        print("Abrir vía aérea")
-        persona = input("¿La persona respira? ")
-        print(persona)
-        if persona == "si":
-             print("Permitale una posición de suficiente respiración")
-             print("Fin ciclo")
-        elif persona == "no":
-             print("Administre 5 ventilaciones y llame a la ambulancia")
-        persona = input("Signos de Vida? ")
-        print(persona)
-        if persona == "no":
-                print("Administrar compresiones toracicas hasta que llegue la ambulancia")
-        elif persona == "si":
-            print("Reevaluar a la espera de la ambulancia")       
-            persona = input("LLegó la ambulancia? ")  
-            print(persona) 
-            if persona == "no":
-                    print("reevaluar nuevamente ¿signos de vida? ")
-            elif persona == "si":
-                      print("Fin del ciclo")
-                     
-
-            
-            
-        
-     
-                      
-
+    print("Abrir vía aérea")
+    respira = input("¿La persona respira? ").lower()
+    print(respira)
+    if respira == "si":
+        print("Permitirle una posición de suficiente respiración")
+        print("Fin ciclo")
+    elif respira == "no":
+        print("Administrar 5 ventilaciones y llamar a la ambulancia")
+        ambulancia = "no"
+        while ambulancia != "si":
+            hay_ambulancia = input("¿Hay signos de vida? ").lower()
+            print(hay_ambulancia)
+            ambulancia = "si"
+            if hay_ambulancia == "no":
+                print("Administrar compresiones torácicas hasta que llegue la ambulancia")
+            elif hay_ambulancia == "si":
+                print("Reevaluar a la espera de la ambulancia") 
+            llega_ambulancia = input("¿Llegó la ambulancia? ").lower()
+            print(llega_ambulancia) 
+            if llega_ambulancia == "no":
+                print("Reevaluar nuevamente ¿hay signos de vida?")
+            elif llega_ambulancia == "si":
+                print("Fin del ciclo")
+            else:
+                print("Debe responder con SI/NO") 
+        else: 
+            print("Debe responder con SI/NO")     
+    else:
+        print("Debe responder con SI/NO")
 else:
     print("Debe responder con SI/NO")
 
